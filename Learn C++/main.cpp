627070d29b3a6246076daa5a1832308c12fb647a
Oyindolapo Komolafe <kikidaope@Oyindolapos-MacBook-Pro.local>
2024-03-09 17:57:54 -0400

    initial

---
+++ Learn C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Learn C++
+//
+//  Created by Oyindolapo Komolafe on 2024-03-09.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
